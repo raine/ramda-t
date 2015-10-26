6a605ab21acd9a0202128c8749438ae67c46cf18
Raine Virta <raine.virta@gmail.com>
2015-10-27 00:25:28 +0200

    don't show ramda source in err context

--- src/format-err-context.js
+++ src/format-err-context.js
@@ -1,7 +1,13 @@
-const { __, addIndex, complement, contains, curry, filter, find, flip, invoker, join, map, max, min, pair, pipe, range, split } = require('ramda')
+const { __, addIndex, anyPass, complement, contains, curry, filter, find, flip, invoker, join, map, max, min, pair, pipe, range, split } = require('ramda')
 const callsites = require('error-callsites')
 const isMyCallSite = require('./is-my-call-site');
 const fs = require('fs')
+
+const getFileName = invoker(0, 'getFileName')
+const includes = curry((x, str) => str.indexOf(x) >= 0)
+const isRamdaSourceFile =
+  pipe(getFileName,
+       includes('node_modules/ramda/dist/ramda.js'))
 
 const lines = split('\n')
 const unwords = join(' ')
@@ -10,10 +16,12 @@ const indexList = mapIndexed(flip(pair))
 const filterIndexed = addIndex(filter)
 
 const firstOuterCallSite =
-  pipe(callsites, find(complement(isMyCallSite)))
+  pipe(callsites, find(complement(anyPass([
+    isMyCallSite,
+    isRamdaSourceFile
+  ]))))
 
 const readFileUtf8 = curry(fs.readFileSync)(__, 'utf8')
-const getFileName = invoker(0, 'getFileName')
 const pickIndexes = curry((idxs, arr) =>
   filterIndexed((val, idx) => contains(idx, idxs), arr))
 
