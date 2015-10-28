f065b2adfa0bed135845a92d5b6516bcfc46950e
Raine Virta <raine.virta@gmail.com>
2015-10-28 23:44:09 +0200

    fallback to ramda-t's ramda

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const { __, any, anyPass, curry, equals, find, identity, ifElse, invoker, mapObjIndexed, not, pipe, prop, propEq, reject, type } = require('ramda')
-const mainRamda = require.main.require('ramda')
+const { __, any, anyPass, curry, equals, find, identity, ifElse, invoker, mapObjIndexed, not, pipe, prop, propEq, reject, toString, type } = require('ramda')
 const path = require('path')
-const docs = require(path.join(__dirname, '..', 'ramda.json'))
 const specialCurryN = require('./special-curryN')
 const nthStr = require('./nth-str')
 const isMyCallSite = require('./is-my-call-site');
@@ -11,9 +9,18 @@ const stackChain = require('stack-chain')
 const debug = require('debug')('ramda-t')
 
 const PACKAGE_ROOT = path.resolve(__dirname, '..')
+const JSON_DOCS = path.join(PACKAGE_ROOT, 'ramda.json')
+const docs = require(JSON_DOCS)
 const when = ifElse(__, __, identity)
 const isFunction = pipe(type, equals('Function'))
 const quote = (x) => `‘${x}’`
+const fallback = curry((def, fn, val) => {
+  try {
+    return fn(val);
+  } catch(e) {
+    return def(val);
+  }
+});
 
 const hasMethod = pipe(prop, isFunction)
 const isVariadic = propEq('variable', true)
@@ -29,9 +36,9 @@ const validArgType = (val, arg) =>
   any(validType(val), arg.types)
 
 const check = curry((fnName, idx, val) => {
-  debug('check', { fnName, idx, val })
   const fn = find(propEq('name', fnName), docs)
   const arg = getArg(fn, idx)
+  debug(`checking ${fnName} idx=${idx} arg=${toString(arg)} val=${toString(val)}`)
 
   if (arg == null)
     return debug(`warning: couldn't find documentation for ${nthStr(idx)} argument of ${quote(fnName)}`)
@@ -46,6 +53,9 @@ const check = curry((fnName, idx, val) => {
 const wrapWithCheck = (fn, name) =>
   specialCurryN(check(name), fn.length, [], fn)
 
+const mainRamda = fallback(require, require.main.require, 'ramda')
+const mainRamdaVersion = fallback(require, require.main.require, 'ramda/package.json').version
+
 module.exports = mapObjIndexed(when(isFunction, wrapWithCheck), mainRamda)
 module.exports.__ = mainRamda.__ // ^ loses this
 
@@ -58,5 +68,8 @@ const isStackNoise = anyPass([
 stackChain.filter.attach((error, frames) =>
   reject(isStackNoise, frames))
 
+Error.stackTraceLimit = 20
+
 debug('ramda-t package root', PACKAGE_ROOT)
-debug('extending ramda version', require.main.require('ramda/package.json').version)
+debug('reading docs json from path', JSON_DOCS)
+debug('ramda version', mainRamdaVersion)
