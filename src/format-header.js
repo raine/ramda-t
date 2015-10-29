567f44228736d9b8276577feafedfd3f329552d0
Raine Virta <raine.virta@gmail.com>
2015-10-29 22:33:28 +0200

    format-header: rename variables

--- src/format-header.js
+++ src/format-header.js
@@ -6,9 +6,10 @@ const TERM_COLUMNS = process.stdout.columns
 const repeatStr = pipe(repeat, join(''))
 
 const formatHeader = (str) => {
-  const a = red('──') + ` ${red.bold(toUpper(str))} `
-  const b = red(repeatStr('─', TERM_COLUMNS - strLen(a)))
-  return concat(a, b)
+  const left = red('──') + ` ${red.bold(toUpper(str))} `
+  const rlen = TERM_COLUMNS - strLen(left)
+  const right = red(repeatStr('─', rlen))
+  return concat(left, right)
 }
 
 module.exports = formatHeader
