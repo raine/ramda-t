9ff14866cb64d428aeefcb9225cf5f4512a68572
Raine Virta <raine.virta@gmail.com>
2015-11-04 22:05:31 +0200

    update scripts/test-ramda.sh

--- scripts/test-ramda.sh
+++ scripts/test-ramda.sh
@@ -8,12 +8,13 @@ if [ ! -d ../ramda/ ]; then
   git clone git@github.com:ramda/ramda.git ../ramda
 fi
 
-(cd ../ramda && git clean -fd && git reset --hard && git checkout v0.18.0)
-rm ../ramda/dist/ramda.js
-rm ../ramda/test/transduce.js
+bad_tests=`cat scripts/bad-tests | paste -s -d"|" -`
+(cd ../ramda && make clean && make)
+cp ../ramda/dist/ramda.js node_modules/ramda/dist/ramda.js
+./bin/generate-json ../ramda/dist/ramda.js ramda.json
 sed -i '' "s/require('..\/..')/require('ramda')/" ../ramda/test/shared/*.js
 sed -i '' "s/require('..')/require('..\/..\/ramda-t\/lib')/" ../ramda/test/*.js
 ./node_modules/.bin/mocha \
   --opts test/mocha-ramda.opts \
-  --invert --grep 'throws|terminates' \
+  --invert --grep "$bad_tests" \
   ../ramda/test/$1
